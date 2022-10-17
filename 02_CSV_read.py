bfaf4c752b5c32f2ef56279277bda3d7eb365897
Mituteruhsu <mituteruiot111a19@gmail.com>
2022-10-17 11:15:23 +0800

    update

--- 02_CSV_read.py
+++ 02_CSV_read.py
@@ -9,11 +9,12 @@ date = df['日期']
 net_worth = df['淨值']
 
 # 繪圖
-plt.plot(date, net_worth)
+plt.plot(date, net_worth, color='#00bd42', linewidth=2)
 plt.xlabel('date')
 plt.ylabel('Net_worth')
 plt.legend(['Net_worth'], loc = 'lower right')
 plt.grid(True)
 
 # 顯示圖片
+plt.savefig("02_CSV_read_chart.png")
 plt.show()
