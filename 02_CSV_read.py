1953e3b791fc3a374c6a77cdc9a201182a9b338d
Mituteruhsu <mituteruiot111a19@gmail.com>
2022-10-18 16:47:18 +0800

    update

--- 02_CSV_read.py
+++ 02_CSV_read.py
@@ -32,6 +32,20 @@ index_d_m10 = df['ID(-10%)']
 index_d_p10 = df['ID(+10%)']
 
 # 繪圖
+# plt.plot(date, net_worth, color='#4473c4', linewidth=1)
+# plt.plot(date, index_deviation, color='#ff0000', linewidth=1)
+# plt.plot(date, index_d_m10, color='#00bd42', linewidth=2)
+# plt.plot(date, index_d_p10, color='#ffbf00', linewidth=2)
+# plt.xlabel('date')
+# plt.ylabel('Net_worth')
+# plt.legend(['Net_worth', 'ID(Index_Deviation)', 'ID(-10%)', 'ID(+10%)'], loc = 'lower right')
+# plt.grid(True)
+
+# 顯示圖片
+# plt.savefig("02_CSV_read_chart.png")
+# plt.show()
+
+plt.subplot(2, 2, 1)
 plt.plot(date, net_worth, color='#4473c4', linewidth=1)
 plt.plot(date, index_deviation, color='#ff0000', linewidth=1)
 plt.plot(date, index_d_m10, color='#00bd42', linewidth=2)
@@ -40,7 +54,39 @@ plt.xlabel('date')
 plt.ylabel('Net_worth')
 plt.legend(['Net_worth', 'ID(Index_Deviation)', 'ID(-10%)', 'ID(+10%)'], loc = 'lower right')
 plt.grid(True)
+plt.title("Test 1", {'fontsize':15})  # 設定圖標題及其文字大小
 
-# 顯示圖片
-# plt.savefig("02_CSV_read_chart.png")
+plt.subplot(2, 2, 2)
+plt.plot(date, net_worth, color='#4473c4', linewidth=1)
+plt.plot(date, index_deviation, color='#ff0000', linewidth=1)
+plt.plot(date, index_d_m10, color='#00bd42', linewidth=2)
+plt.plot(date, index_d_p10, color='#ffbf00', linewidth=2)
+plt.xlabel('date')
+plt.ylabel('Net_worth')
+plt.legend(['Net_worth', 'ID(Index_Deviation)', 'ID(-10%)', 'ID(+10%)'], loc = 'lower right')
+plt.grid(True)
+plt.title("Test 2", {'fontsize':15})  # 設定圖標題及其文字大小
+
+plt.subplot(2, 2, 3)
+plt.plot(date, net_worth, color='#4473c4', linewidth=1)
+plt.plot(date, index_deviation, color='#ff0000', linewidth=1)
+plt.plot(date, index_d_m10, color='#00bd42', linewidth=2)
+plt.plot(date, index_d_p10, color='#ffbf00', linewidth=2)
+plt.xlabel('date')
+plt.ylabel('Net_worth')
+plt.legend(['Net_worth', 'ID(Index_Deviation)', 'ID(-10%)', 'ID(+10%)'], loc = 'lower right')
+plt.grid(True)
+plt.title("Test 3", {'fontsize':15})  # 設定圖標題及其文字大小
+
+plt.subplot(2, 2, 4)
+plt.plot(date, net_worth, color='#4473c4', linewidth=1)
+plt.plot(date, index_deviation, color='#ff0000', linewidth=1)
+plt.plot(date, index_d_m10, color='#00bd42', linewidth=2)
+plt.plot(date, index_d_p10, color='#ffbf00', linewidth=2)
+plt.xlabel('date')
+plt.ylabel('Net_worth')
+plt.legend(['Net_worth', 'ID(Index_Deviation)', 'ID(-10%)', 'ID(+10%)'], loc = 'lower right')
+plt.grid(True)
+plt.title("Test 4", {'fontsize':15})  # 設定圖標題及其文字大小
+
 plt.show()
--- 02_CSV_read_test_note.py
+++ 02_CSV_read_test_note.py
@@ -98,16 +98,83 @@ df = pd.read_csv('TLZ64_0928.csv')
 
 # ===============================================
 # 刪除列的方式
-del df['漲跌幅(%)'] #可刪除列
+# del df['漲跌幅(%)'] #可刪除列
 # df.drop(columns='漲跌幅(%)')
 # df.pop('漲跌幅(%)')
 
 # 移動平均線
 # 我們分別計算7天,15天與30天的移動平均線
 # df['MA_7'] = <- 會直接新增列
-df['MA_7'] = df['淨值'].rolling(7, min_periods=1).mean()
-df['MA_15'] = df['淨值'].rolling(15, min_periods=1).mean()
-df['MA_30'] = df['淨值'].rolling(30, min_periods=1).mean()
+# df['MA_7'] = df['淨值'].rolling(7, min_periods=1).mean()
+# df['MA_15'] = df['淨值'].rolling(15, min_periods=1).mean()
+# df['MA_30'] = df['淨值'].rolling(30, min_periods=1).mean()
 
-print(df)
+# print(df)
 # df.to_csv('TLZ64_0928_MA.csv', index=False)
+
+# =====================================================
+# 1. 載入套件。
+
+import numpy as np                # 資料處理套件
+import pandas as pd               # 資料處理套件
+import matplotlib.pyplot as plt   # 資料視覺化套件
+# 2. 載入資料。
+
+TRA = pd.read_csv("passenger number of TRA(105-106).csv")
+TRA.head(3)    # 顯示前3筆資料
+
+# 3. 資料操作。
+# 由於原資料的載客數單位為人次，為利視覺化結果簡潔，將載客數改為百萬計
+
+pr = [None] * len(TRA["passenger"])       # 建立一個空列表，數量為載客數欄位資料的數量
+
+# 以for迴圈逐一將載客數資料自左向右取5個字元
+for i in range(len(TRA["passenger"])):     
+    pr[i] = TRA["passenger"][i][:5]
+
+# 以for迴圈逐一將載客數資料中的","替換成"."
+for i in range(len(pr)):
+    pr[i] = pr[i].replace(",", ".")
+    
+# 將原資料替換掉，並重新命名欄位名稱  
+TRA["passenger"] = pr
+TRA.rename(columns = {"passenger" : "passenger(million)"}, inplace = True)
+
+
+# 4. 開始畫圖。
+
+year = str(np.unique(TRA["year"]))   # 將年份資料單獨取出
+year = year.split(" ")               # 以空白字元分隔年分資料
+
+# 畫第一張圖，位於左上角
+# 其中pd.DataFrame(資料,dtype=np.float)為將載客數字串類別更改為float類別
+# 其中TRA[TRA.station == "Taipei"]["passenger(million)"]為擷取出台北站資料，再擷取出載客數欄位資料
+plt.subplot(2, 2, 1)                 # plt.subplot(列數, 行數, 圖形編號)設定第一張圖位置
+plt.plot(year, pd.DataFrame(TRA[TRA.station == "Taipei"]["passenger(million)"],dtype=np.float)
+        ,"r")                         # plt.plot(x軸資料, y軸資料, 線顏色)
+plt.title("Taipei", {'fontsize':15})  # 設定圖標題及其文字大小
+
+# 畫第二張圖，位於右上角
+plt.subplot(2, 2, 2)
+plt.plot(year, pd.DataFrame(TRA[TRA.station == "Taoyuan"]["passenger(million)"],dtype=np.float)
+        ,"g--")
+plt.title("Taoyuan", {'fontsize':15})
+
+# 畫第三張圖，位於左下角
+plt.subplot(2, 2, 3)
+plt.plot(year, pd.DataFrame(TRA[TRA.station == "Zhongli"]["passenger(million)"],dtype=np.float)
+        ,"b-.")
+plt.title("Zhongli", {'fontsize':15})
+
+# 畫第四張圖，位於右下角
+plt.subplot(2, 2, 4)
+plt.plot(year, pd.DataFrame(TRA[TRA.station == "Tainan"]["passenger(million)"],dtype=np.float)
+        ,"m:")
+plt.title("Tainan", {'fontsize':15})        
+        
+
+# 儲存圖檔
+plt.savefig("Passengers of the top 4 TRA station.jpg",   # 儲存圖檔
+            bbox_inches='tight',               # 去除座標軸占用的空間
+            pad_inches=0.0)                    # 去除所有白邊
+plt.close()      # 關閉圖表      
